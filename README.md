c51b186ba815eacb6f94260cd1eb0a122a732043
Grant McAleese <grantmcaleese@hotmail.com>
2018-10-29 18:52:09 +0000

    tested google maps autocomplete code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 website design idea
 http://psd.la/startravel/# Project-2-Interactive-Frontend-Development-Milestone-Project
+
+navbar 
+https://getbootstrap.com/docs/3.3/components/#navbar
+
+
+Place Types
+https://developers.google.com/places/supported_types#table3
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,8 +1,8 @@
  function initMap(){
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 2,
+        zoom: 1.7,
         center: {
-            lat: 46.619261,
+            lat: 17.619261,
             lng: 20.134766
         }
     }); 
@@ -10,8 +10,21 @@
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
                    
     var locations = [
-                     
+         
+        { lat: 55.863801, lng: -4.251661 }, //  Glasgow central, Scotland
+        { lat: 55.876348, lng: -4.297794 }, // Glasgow West, Scotland
+        { lat: 55.953080, lng: -3.189861 }, // Edinburgh, Scotland
+        { lat: 48.855652, lng:  2.350145 },  // Paris, France
+        { lat: 43.710590, lng:  7.259350 },  // Nice, France
+        { lat: 40.413774, lng:  -3.707047 },  // Madrid, Spain
+        { lat: 41.901770, lng:  12.491347 },  // Rome, Italy
+        { lat: 45.439899, lng:  12.318674 },  // Venice, Italy
+        { lat: 40.707299, lng: -74.012615 }, // New York, America
+        { lat: 36.485994, lng: -119.825945 }, // California, America
+        { lat: 27.764441, lng: -81.823893 },  // Florida, America
+        { lat: -33.869531, lng: 151.201544 }  // Sydney, Australia
     ];
+    
                    
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
